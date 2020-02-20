7007f567820013d99d83d39d830f066bff674e5d
Jefferson Botelho <jgbotelho3@outlook.com>
2020-02-19 23:52:40 -0300

    set form style and product routes

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const ProductController = require('../app/controllers/ProductController')
 
 router.get('/', (req, res) => {
   return res.render('layout.njk')
+})
+
+router.get('/products/create', ProductController.create)
+
+router.get('/ads/create', (req, res) => {
+  return res.redirect('/products/create')
 })
 
 
