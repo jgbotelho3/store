f57acc488cb4b639cd37b4c089c5d1603c2458ef
Jefferson Botelho <jgbotelho3@outlook.com>
2020-02-27 02:31:46 -0300

    search finished

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,6 +1,11 @@
 const db = require('../../config/db')
 
 module.exports = {
+
+  all(){
+    return db.query(`SELECT * FROM products ORDER BY updated_at DESC`)
+  },
+
   create (data) {
   
     const query = `
@@ -76,5 +81,36 @@ module.exports = {
   files(product_id){
 
     return db.query(`SELECT * FROM files WHERE product_id = $1`, [product_id])
+  },
+
+  search(params){
+    const { filter, category } = params
+
+    let query = '',
+    filterQuery = `WHERE`
+
+    if(category){
+      filterQuery = `
+        ${filterQuery}
+        products.category_id = ${category}
+        AND
+      `
+    }
+
+    filterQuery = `
+      ${filterQuery} 
+      products.name ILIKE '%${filter}%'
+      OR products.description ILIKE '%${filter}%'
+    `
+
+    query = `
+      SELECT products.*,
+      categories.name AS category_name
+      FROM products
+      LEFT JOIN categories ON (categories.id = products.category_id)
+      ${filterQuery}
+    `
+
+    return db.query(query)
   }
 }
--- src/route/routes.js
+++ src/route/routes.js
@@ -2,11 +2,16 @@ const express = require('express')
 const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 const ProductController = require('../app/controllers/ProductController')
+const HomeController = require('../app/controllers/HomeController')
+const SearchController = require('../app/controllers/SearchController')
 
-routes.get('/', (req, res) => {
-  return res.render('layout.njk')
-})
+//Home
+routes.get('/', HomeController.index)
 
+//Search
+routes.get('/products/search', SearchController.index)
+
+//Products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
@@ -15,6 +20,7 @@ routes.put('/products', multer.array('photos', 6), ProductController.put)
 routes.delete('/products', ProductController.delete)
 
 
+//Alias
 routes.get('/ads/create', (req, res) => {
   return res.redirect('/products/create')
 })
